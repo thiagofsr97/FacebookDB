d554083466943f9f9774d56b6f652c46799ef7a4
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2018-06-12 00:48:27 -0300

    Erro while trying to respond comment solved

--- commentRoutes.js
+++ commentRoutes.js
@@ -15,7 +15,7 @@ router.post('/post/:post_id?/comment',(req,res)=>{
     const sqlQuery_2 = 'UPDATE Post '+
             'SET Post.NumberOfComments = Post.NumberOfComments + 1 ' +
             `WHERE (Post.idPost = '${post_id}') AND (Post.UserProfile_idUserProfile_postOwner = '${post_owner}');`
-    
+
     utils.queryTransaction([sqlQuery_1,sqlQuery_2],res);
 
 
@@ -37,7 +37,7 @@ router.get('/post/:post_id?/comments',(req,res)=>{
 
 router.get('/post/:post_id?/comments/:comment_id?/delete',(req,res)=>{
     const post_id = req.params.post_id;
-    const comment_id = req.params.comment_id;    
+    const comment_id = req.params.comment_id;
     const sqlQuery_1 = `DELETE FROM Comments WHERE idComments = '${comment_id}';`
     const sqlQuery_2 = `UPDATE Post SET NumberOfComments = NumberOfComments - 1 WHERE idPost = '${post_id}';`
 
@@ -53,7 +53,7 @@ router.post('/post/:post_id?/comment/:comment_id/respond',(req,res)=>{
     const user_responder_id = req.body.user_id_responder;
     const response = req.body.text;
     const sqlQuery_1 = `INSERT INTO Responses(ResponseTime, Comments_idComments, Comments_Post_idPost, Comments_Post_UserProfile_idUserProfile_postOwner, Comments_UserProfile_idUserProfile_commenter, UserProfile_idUserProfile_responder, text) ` +
-    `VALUES ('${moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')}','${comment_id}','${post_id}','${post_owner}','${post_owner}','${user_commenter_id}','${user_id_responder}','${response}')`;
+    `VALUES ('${moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')}','${comment_id}','${post_id}','${post_owner}','${user_commenter_id}','${user_responder_id}','${response}')`;
     const sqlQuery_2 = `UPDATE Comments SET NumberOfResponses = NumberOfResponses + 1 WHERE idComments = '${comment_id}';`
 
     utils.queryTransaction([sqlQuery_1,sqlQuery_2],res);
@@ -71,18 +71,18 @@ router.get('/comments/:comment_id?/responses',(req,res)=>{
 
 // ----------------------------- Listing responses from a post-----------------------------------//
 
-router.get('/comments/:comment_id?/responses',(req,res)=>{
-    const comment_id = req.params.comment_id;
-    const sqlQuery = `SELECT * FROM Responses WHERE Comments_idComments = '${comment_id}'`;
-    utils.querySQL(sqlQuery,res);
-
-});
+// router.get('/comments/:comment_id?/responses',(req,res)=>{
+//     const comment_id = req.params.comment_id;
+//     const sqlQuery = `SELECT * FROM Responses WHERE Comments_idComments = '${comment_id}'`;
+//     utils.querySQL(sqlQuery,res);
+//
+// });
 
 // ----------------------------- Deleting a comment on a post -----------------------------------//
 
 router.get('/comments/:comment_id?/responses/:response_id?/delete',(req,res)=>{
     const response_id = req.params.response_id;
-    const comment_id = req.params.comment_id;    
+    const comment_id = req.params.comment_id;
     const sqlQuery_1 = `DELETE FROM Responses WHERE idResponses = '${response_id}';`
     const sqlQuery_2 = `UPDATE Comments SET NumberOfResponses = NumberOfResponses - 1 WHERE idComments = '${comment_id}';`
 
